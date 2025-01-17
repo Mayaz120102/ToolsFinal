f5ecf52ef79224e2faa14b0c4d2e499b0b7729c4
Mayaz120102 <abrarmayaz2002@gmail.com>
2025-01-17 12:34:09 +0600

    allpage added

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import First_Page from './Components/First_Page';  // Remove the leading slash
+import NavBar from './Components/NavBar'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+// import Navigation from './Components/Navigation'
+// import Drawing from './Components/Drawing'
+// import Music from './Components/Music'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <NavBar/>
+      <Navigation/>
+
+      <Router>
+        <Routes>
+          <Route path = "" element={<First_Page/>}/>
+          <Route path = "music/" element={<Music/>}/>
+          <Route path = "drawing/" element={<Drawing/>}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
